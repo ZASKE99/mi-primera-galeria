f3589bbbc771acd786604be8b4fa8face57ca997
ZASKE99 <albertoromerohuerta@gmail.com>
2023-04-26 02:25:30 -0600

    primer commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// images
+let images = document.querySelectorAll('img')
+// wrapper
+let wrapper = document.getElementById('wrapper')
+// imgwrapper
+let imgWrapper = document.getElementById('fullImg')
+// close
+let close = document.querySelector('span')
+
+images.forEach((img, index) => {
+    img.addEventListener('click', () => {
+        openModal(`images/${index}.jpg`)
+    })
+})
+
+close.addEventListener('click', () => (wrapper.style.display = 'none'));
+
+openModal = (pic)  =>  {
+    wrapper.style.display = 'flex'
+    imgWrapper.src = pic
+}
